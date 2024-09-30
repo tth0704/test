8331fef99e83bc61fe3bfeebcc5bbf49a47d7be5
tth0704 <tth0704@gmail.com>
2024-09-30 15:07:27 +0700

    test 3000t

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use('/test', function(req, res){ 
-  console.log(req.ip.split(':').pop());
-  res.status(200).json(`Server is running ${req.ip}`)
+  res.status(200).json({ip: req.ip.split(':').pop()})
 });
 
 const PORT = process.env.PORT || 3000;
