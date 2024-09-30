1acd1f3f529194acb43e91c26975c4fba0a0b0a6
tth0704 <tth0704@gmail.com>
2024-09-30 10:23:43 +0700

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use('/', function(req, res){ 
+  console.log(req.ips);
+  res.status(200).json(`Server is running ${req.ips}`)
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
