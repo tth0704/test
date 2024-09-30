cb629b17c08c083b8d60330ab4cf4c86cc3a71a3
tth0704 <tth0704@gmail.com>
2024-09-30 15:04:16 +0700

    test 3000t

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use('/test', function(req, res){ 
-  console.log(req.ip);
+  console.log(req.ip.split(':').pop());
   res.status(200).json(`Server is running ${req.ip}`)
 });
 
